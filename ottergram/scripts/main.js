d1190d92ca32b9aeb05adeb78c039ed8c10e1299
Nathan Yee <nyee@csu.fullerton.edu>
2020-09-08 21:50:12 -0700

    added main.js and changed links for thumbnail img

---
+++ ottergram/scripts/main.js
@@ -0,0 +1,40 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails() {
+    'use strict';
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageURL);
+
+    var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailImage.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function titleFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+    'use strict';
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+    'use strict';
+    thumb.addEventListener('click', function (event) {
+        event.preventDefault();
+        setDetailsFromThumb(thumb);
+    });
+}
+
+function getThumbnailsArray() {
+    'use strict';
+    var thumbnails = document.querySelector(THUMBNAIL_LINK_SELECTOR);
+}
